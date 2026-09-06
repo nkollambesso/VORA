12d4a1d0bd18c0717e277ce65c6f107db1c15ab4
psycho237-prog <legrand.onana@facsciences-uy1.cm>
2026-09-06 10:26:13 +0100

    fix(metro): add web.js sourceExts to metro.config.js to fix React Native Web resolution on Windows
    Co-authored-by: Patrick Assako <patrickassako@users.noreply.github.com>

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,4 +2,18 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts = [
+  "web.tsx",
+  "web.ts",
+  "web.jsx",
+  "web.js",
+  "tsx",
+  "ts",
+  "jsx",
+  "js",
+  "json",
+  "cjs",
+  "mjs",
+];
+
 module.exports = config;
