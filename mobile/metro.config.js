e314715acdef96b97b310d5c9066d0c175308353
psycho237-prog <legrand.onana@facsciences-uy1.cm>
2026-09-06 10:53:18 +0100

    fix(metro): set web extensions and platforms resolution in metro.config.js
    Co-authored-by: Patrick Assako <patrickassako@users.noreply.github.com>

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,4 +2,20 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.sourceExts = [
+  "web.tsx",
+  "web.ts",
+  "web.jsx",
+  "web.js",
+  "tsx",
+  "ts",
+  "jsx",
+  "js",
+  "json",
+  "cjs",
+  "mjs",
+];
+
+config.resolver.platforms = ["web", "ios", "android"];
+
 module.exports = config;
